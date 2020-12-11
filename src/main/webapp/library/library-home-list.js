b3602fbb9782ee6da512abf647d9d6841db762d0
gabbybruck <gabby.bruck@gmail.com>
2020-12-11 11:47:45 -0500

    library home

---
+++ src/main/webapp/library/library-home-list.js
@@ -0,0 +1,37 @@
+class LibraryList extends React.Component {
+   state = {
+      libraries: []
+   }
+
+   findAllLibraries = () =>
+      findAllLibraries()
+         .then(libraries => this.setState({libraries}))
+
+   componentDidMount = () =>
+      this.findAllLibraries()
+
+ render() {
+   return(
+     <div className="container">
+       <h1>Select your library</h1>
+       <table>
+             {
+                this.state.libraries.map(library =>
+                <tr key={library.id}>
+                   <td>
+                   <a href="patron-or-librarian.html">
+                   {library.name}
+                   </a>
+                   </td>
+                </tr>
+                )
+             }
+             </table>
+
+     </div>
+   )
+ }
+}
+
+ReactDOM.render(
+ <LibraryList/>, document.getElementById("root"))
