26966dca0827601330b0c1f341018d1cd3016850
rimmereidl <72942725+rimmereidl@users.noreply.github.com>
2020-12-12 16:13:13 +0100

    Update patron-list.js

--- src/main/webapp/patron/patron-list.js
+++ src/main/webapp/patron/patron-list.js
@@ -1,8 +1,61 @@
-class PatronList extends React.Component {
+class BookList extends React.Component {
+
+    state = {
+        patrons: []
+    }
+
+    findAllPatrons = () =>
+        findAllPatrons()
+            .then(patrons => this.setState({patrons}))
+
+    componentDidMount = () =>
+        this.findAllPatrons()
+
+    renamePhoneNumber = (id, newPhone) =>
+        renamePhoneNumber(id, newPhone)
+            .then(this.findAllPatrons)
+
+    renameAddress = (id, newAddress) =>
+        renameAddress(id, newAddress)
+            .then(this.findAllPatrons)
+
+    deletePatron = (id) =>
+        deletePatron(id)
+            .then(this.findAllBooks)
+
     render() {
         return(
             <div>
                 <h1>Patrons</h1>
+                <table>
+                    <tbody>
+                    {
+                        this.state.books.map(patron =>
+                            <tr>
+                                <td>
+                                    {patron.id}
+
+                                </td>
+                                <td>
+                                    <a href={`item-list.html?id=${patron.id}`}>
+                                        View Items
+                                    </a>
+                                </td>
+                                <td>
+                                    <button onClick={() => this.deletePatron(patron.id)}>
+                                        Delete
+                                    </button>
+                                </td>
+                                <td>
+                                    <a href={`Patron-editor.html?id=${patron.id}`}>
+                                        Edit
+                                    </a>
+                                </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
             </div>
         )
     }
