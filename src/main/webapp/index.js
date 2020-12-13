ec45052a48331d6ebfeaefd724854cf0b6560bc5
gabbybruck <gabby.bruck@gmail.com>
2020-12-13 14:59:22 -0500

    view account by patron

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,4 +1,45 @@
+
+class LibraryList extends React.Component {
+   state = {
+      libraries: []
+   }
+
+   findAllLibraries = () =>
+      findAllLibraries()
+         .then(libraries => this.setState({libraries}))
+
+   byLibrary = (id) =>
+             byLibrary(id)
+               .then(this.byLibraries)
+
+   componentDidMount = () =>
+      this.findAllLibraries()
+
+ render() {
+   return(
+     <div className="container">
+       <h2>Select your library</h2>
+       <table>
+             {
+                this.state.libraries.map(library =>
+                <tr key={library.id}>
+                   <td>
+
+                   <a href="/library/patron-or-librarian.html">
+                       {library.name}
+                       </a>
+
+                   </td>
+
+                </tr>
+                )
+             }
+             </table>
+
+     </div>
+   )
+ }
+}
+
 ReactDOM.render(
-  <CourseList />,
-  document.getElementById('root')
-)
+ <LibraryList/>, document.getElementById("root"))
