71c5e566b990853709444a30b34a98fc9fb5acaf
rimmereidl <72942725+rimmereidl@users.noreply.github.com>
2020-12-14 16:18:52 +0100

    account-list-viewer (no edit)

---
+++ src/main/webapp/accounts/account-list-view.js
@@ -0,0 +1,62 @@
+class AccountList extends React.Component {
+
+    state = {
+        accounts: []
+    }
+
+    findAllAccounts = () =>
+        findAllAccounts()
+            .then(accounts => this.setState({accounts}))
+
+    componentDidMount = () =>
+        this.findAllAccounts()
+
+    createAccount = () =>
+        createAccount()
+            .then(this.findAllAccounts)
+
+    deleteAccount = (id) =>
+        deleteAccount(id)
+            .then(this.findAllAccounts)
+
+    render() {
+        return(
+            <div>
+                <h1>Account List</h1>
+
+
+                <table>
+                    <tbody>
+                    {
+                        this.state.accounts.map(account =>
+                            <tr>
+                                <td>
+                                    <th>Acct</th>{account.number} &nbsp;
+                                </td>
+                                <td>
+                                    <th>State</th>{account.state} &nbsp;
+                                </td>
+                                <td>
+                                    <th>Item 1</th>{account.item1} &nbsp;
+                                </td>
+                                <td>
+                                    <th>Item 2</th>{account.item2} &nbsp;
+                                </td>
+                                <td>
+                                    <th>Item 3</th>{account.item3} &nbsp;
+                                </td>
+
+
+
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <AccountList/>, document.getElementById("root"))
