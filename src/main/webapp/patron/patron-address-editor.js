49c5cf877c3d4d9faefe0a7041913516c372bc37
gabbybruck <gabby.bruck@gmail.com>
2020-12-14 18:39:38 -0500

    address editor

--- src/main/webapp/patron/patron-address-editor.js
+++ src/main/webapp/patron/patron-address-editor.js
@@ -1,4 +1,4 @@
-class PatronPhoneEditor extends React.Component {
+class PatronAddressEditor extends React.Component {
 
     state = {
         patron: {}
@@ -19,7 +19,7 @@ class PatronPhoneEditor extends React.Component {
     render() {
         return(
             <div className="container">
-                <h1>Phone Number Editor {this.state.patron.id}</h1>
+                <h1>Phone Address Editor {this.state.patron.id}</h1>
                 <input className="form-control" readOnly={true} value={this.state.patron.id}/>
                 <input
                     onChange={(event) => this.setState({
@@ -40,3 +40,6 @@ class PatronPhoneEditor extends React.Component {
         )
     }
 }
+
+ReactDOM.render(
+    <PatronAddressEditor/>, document.getElementById("root"))
--- src/main/webapp/patron/patron-view.js
+++ src/main/webapp/patron/patron-view.js
@@ -2,8 +2,8 @@ class PatronView extends React.Component {
         render() {
         return(
             <div>
-                <h1><a href="../book/book-list-patron.html">View Catalog Here</a></h1>
-                <h1><a href="../patron/patron-list.html">View Account Info Here</a></h1>
+                <h1 ><a href="../book/book-list-patron.html">View Catalog Here</a></h1>
+                <h1 ><a href="../patron/patron-list.html">View Account Info Here</a></h1>
 
             </div>
         )
